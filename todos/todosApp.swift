a48596123d12abae5af9d8772039cc4e806f4d4c
Chrystian Guth <csguth@gmail.com>
2021-04-11 09:52:33 +0200

    Initial Commit

---
+++ todos/todosApp.swift
@@ -0,0 +1,17 @@
+//
+//  todosApp.swift
+//  todos
+//
+//  Created by Chrystian Guth on 11/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct todosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
